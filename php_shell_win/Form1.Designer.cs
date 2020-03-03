5bb64b235fdf35b5deab8da5234716019a605dd4
ding <huangdingding@joyoung.com>
2020-03-03 15:47:17 +0800

    添加项目文件。

---
+++ php_shell_win/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace php_shell_win
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(-1, -1);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(886, 764);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(884, 761);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "PHP shell for win";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ php_shell_win/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace php_shell_win
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.textBox1.Focus();
+            this.textBox1.Text = "php> ";
+            this.textBox1.Select(this.textBox1.Text.Length, 0);
+
+            //设置Form窗体初始化放在屏幕中间
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            //设置Form窗口透明度
+            this.Opacity = 0.6;
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) {
+
+                //Console.WriteLine(textBox1.Text[textBox1.Text.Length - 2] == (char)13);
+                //Console.WriteLine(textBox1.Text[textBox1.Text.Length - 1] == (char)10);
+                //Console.WriteLine(this.textBox1.GetFirstCharIndexFromLine(0));
+
+                //按回车前的光标位置
+                int line_last = textBox1.GetFirstCharIndexOfCurrentLine() - 2;
+                //int line_first = textBox1.GetFirstCharIndexFromLine(line_last);
+
+                int line_first = line_last;
+                char c = textBox1.Text[line_first];
+                //c == (char)13
+                //Console.WriteLine(line_first+" "+textBox1.Text.Length);
+                //Console.WriteLine(textBox1.Text[line_first]);
+                //Console.WriteLine(textBox1.Text[0]);
+                while (line_first > 0 && c != (char)13)
+                {
+                    line_first--;
+                    c = textBox1.Text[line_first];
+                }
+
+                //
+                //int line = textBox1.GetLineFromCharIndex(index);
+                Console.WriteLine(line_first + " " + line_last);
+            }
+        }
+    }
+}
